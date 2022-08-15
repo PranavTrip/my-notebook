84f26984e000aa4911a0086c58d74624b7b713e2
Pranav <pranavtri00@gmail.com>
2022-08-15 20:31:10 +0530

    Fixed auth API

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -102,7 +102,7 @@ router.post("/getUser",fetchUser, async (req, res) => {
   try {
     userId=req.user.id;
     const user=await User.findById(userId).select("-password");
-    res.send(user);
+    res.json({user});
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Internal Server Error");
