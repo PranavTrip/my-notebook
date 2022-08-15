6b976783aa75305c42b94320b23ff690ceb90695
Pranav <pranavtri00@gmail.com>
2022-08-16 03:20:43 +0530

    Fetch all notes

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,8 +1,17 @@
-const express=require('express');
-const router=express.Router();
+const express = require("express");
+const fetchUser = require("../middleware/fetchUser");
+const Notes = require("../models/Notes");
+const User = require("../models/user");
+const router = express.Router();
+const {vody,validationResult}=require ("express-validator")
+// Route 1 : Fetch all notes of logged in users using /api/notes/fetchallusers . Login required.
+router.get("/fetchallnotes", fetchUser, async (req, res) => {
+  try {
+    const notes = Notes.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    res.status(500).send("Some Error Occured");
+  }
+});
 
-router.get('/',(req,res)=>{
-    res.send('Notes API');
-})
-
-module.exports=router;
+module.exports = router;
